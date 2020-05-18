a5bc39552aa2990a3f42ea93af63f03a0ae32373
Maj Roslyng Larsen <majroslynglarsen@Majs-MacBook-Air.local>
2020-05-18 12:31:12 +0200

    navbar in progress

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    //    document.querySelector(".navbar-toggler").classList.toggle("change");
+
+    var element = document.getElementsByClassName(".navbar-toggler");
+    element.classList.toggle("change");
+
+}
