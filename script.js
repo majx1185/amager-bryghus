4eeb1cebdba8f4d5e840b864b7ad1aef3cbb7c4c
Maj Roslyng Larsen <majroslynglarsen@Majs-MacBook-Air.local>
2020-05-21 14:42:28 +0200

    grov opsætning, mangler et par sider. Ingen design

--- script.js
+++ script.js
@@ -14,3 +14,35 @@ $(document).ready(function () {
         $('body').toggleClass('nav-open');
     });
 });
+
+
+let feed = [];
+
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {
+    getJson();
+}
+
+async function getJson() {
+    let url =
+        "http://majroslyng.dk/kea/eksamen4/wordpress/wp-json/wp/v2/posts"
+    let jsonData = await fetch(url);
+    feed = await jsonData.json();
+    visFeed();
+}
+
+function visFeed() {
+    let temp = document.querySelector("template");
+    let dest = document.querySelector(".beerscontainer")
+
+    feed.forEach(onepost => {
+        console.log(onepost);
+
+        let klon = temp.cloneNode(true).content;
+        klon.querySelector("img").src = onepost.guid;
+
+        klon.querySelector("a").href = "eachpic.html?slug=" + onepost.slug;
+        dest.appendChild(klon);
+    });
+}
