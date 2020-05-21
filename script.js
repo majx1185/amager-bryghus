ed1151c016648bffc1b8474fdc2397f46cd5e5c5
Maj Roslyng Larsen <majroslynglarsen@Majs-MacBook-Air.local>
2020-05-21 10:32:08 +0200

    21/05 - 3-4 sider grove wireframes

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function toggleMenu() {
-    //    document.querySelector(".navbar-toggler").classList.toggle("change");
+//function toggleMenu() {
+//    let erSkjult =
+//        document.querySelector(".navbar-toggler").classList.toggle("change");
+//
+//
+//}
 
-    var element = document.getElementsByClassName(".navbar-toggler");
-    element.classList.toggle("change");
+//function changeFunction(x) {
+//    x.classList.toggle("change");
+//}
 
-}
+$(document).ready(function () {
+    $('.nav-button').click(function () {
+        $('body').toggleClass('nav-open');
+    });
+});
